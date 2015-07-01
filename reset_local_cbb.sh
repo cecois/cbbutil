58ba3177503b4e14a7055a0de054c16364cffe7b
= <cecmcgee@gmail.com>
2015-07-01 13:30:04 -0400

    updated bits schema and a csv export from Refined bits

--- reset_local_cbb.sh
+++ reset_local_cbb.sh
@@ -5,7 +5,7 @@ echo "killing cbb_bits..."
 curl http://localhost:8983/solr/cbb_bits/update --data '<delete><query>*:*</query></delete>' -H 'Content-type:text/xml; charset=utf-8' && curl http://localhost:8983/solr/cbb_bits/update --data '<commit/>' -H 'Content-type:text/xml; charset=utf-8'
 
 echo "killing cbb_carto..."
-curl http://localhost:8983/solr/cbb_carto/update --data '<delete><query>*:*</query></delete>' -H 'Content-type:text/xml; charset=utf-8' && curl http://localhost:8983/solr/cbb_carto/update --data '<commit/>' -H 'Content-type:text/xml; charset=utf-8'
+# curl http://localhost:8983/solr/cbb_carto/update --data '<delete><query>*:*</query></delete>' -H 'Content-type:text/xml; charset=utf-8' && curl http://localhost:8983/solr/cbb_carto/update --data '<commit/>' -H 'Content-type:text/xml; charset=utf-8'
 
 
 
@@ -14,17 +14,18 @@ curl http://localhost:8983/solr/cbb_carto/update --data '<delete><query>*:*</que
 #	TABLES UNION WITH INVALID GEOJSON -----!!!!
 # echo "skipping cbb_carto..."
 echo "refilling cbb_carto..."
-curl "https://pugo.cartodb.com/api/v1/sql?q=select%20cartodb_id%20as%20cartodb_id,name,confidence,anno,created_at,updated_at,ST_AsGeoJSON(the_geom)%20as%20the_geom,'point'%20as%20geom_type%20from%20cbb_point%20UNION%20ALL%20select%20cartodb_id*999%20as%20cartodb_id,name,confidence,anno,created_at,updated_at,ST_AsGeoJSON(the_geom)%20as%20the_geom,'line'%20as%20geom_type%20from%20cbb_line%20UNION%20ALL%20select%20cartodb_id*9999%20as%20cartodb_id,name,confidence,anno,created_at,updated_at,ST_AsGeoJSON(the_geom)%20as%20the_geom,'poly'%20as%20geom_type%20from%20cbb_poly" -o /tmp/pcdbraw.json;
+# curl "https://pugo.cartodb.com/api/v1/sql?q=select%20cartodb_id%20as%20cartodb_id,name,confidence,anno,created_at,updated_at,ST_AsGeoJSON(the_geom)%20as%20the_geom,'point'%20as%20geom_type%20from%20cbb_point%20UNION%20ALL%20select%20cartodb_id*999%20as%20cartodb_id,name,confidence,anno,created_at,updated_at,ST_AsGeoJSON(the_geom)%20as%20the_geom,'line'%20as%20geom_type%20from%20cbb_line%20UNION%20ALL%20select%20cartodb_id*9999%20as%20cartodb_id,name,confidence,anno,created_at,updated_at,ST_AsGeoJSON(the_geom)%20as%20the_geom,'poly'%20as%20geom_type%20from%20cbb_poly" -o /tmp/pcdbraw.json;
 #	WINNER-----!!!!
- jq .rows /tmp/pcdbraw.json > /tmp/pcdb.json
+ # jq .rows /tmp/pcdbraw.json > /tmp/pcdb.json
 #	WINNER (LOCAL)-----!!!!
- curl 'http://localhost:8983/solr/cbb_carto/update/json?commit=true' --data-binary @/tmp/pcdb.json -H 'Content-type:application/json'
+ # curl 'http://localhost:8983/solr/cbb_carto/update/json?commit=true' --data-binary @/tmp/pcdb.json -H 'Content-type:application/json'
 
  # ------------------ BITS
  # 
 echo "refilling cbb_bits..."
  # #	WINNER-----!!!!
-mongoexport -h ds033599.mongolab.com:33599 --db cbbbits --collection bits -u cecmcgee -p 5NWpI1 --fields '_id,show,episode,slug_earwolf,id_wikia,url_soundcloud,name,desc,elucidation,tags,tstart,tend,location_type,location_id,holding' --jsonArray --out /tmp/bits_incoming.json
+# mongoexport -h ds033599.mongolab.com:33599 --db cbbbits --collection bits -u cecmcgee -p 5NWpI1 --fields '_id,show,episode,slug_earwolf,id_wikia,url_soundcloud,name,desc,elucidation,tags,tstart,tend,location_type,location_id,holding' --jsonArray --out /tmp/bits_incoming.json
+mongoexport --db cbbbits --collection bits --fields '_id,show,episode,slug_earwolf,id_wikia,url_soundcloud,name,desc,elucidation,tags,tstart,tend,location_type,location_id,holding' --jsonArray --out /tmp/bits_incoming.json
 #	WINNER-----!!!!
 jq --compact-output '[.[]|{_id:._id."$oid",show:.show,episode:.episode,slug_earwolf:.slug_earwolf,id_wikia:.id_wikia,url_soundcloud:.url_soundcloud,name:.name,desc:.desc,elucidation:.elucidation,tags:.tags,tstart:.tstart,tend:.tend,location_type:.location_type,location_id:.location_id,holding:.holding}]' < /tmp/bits_incoming.json > /tmp/bits_jqd.json
 
