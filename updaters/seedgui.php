1c8c693b3563d9a46e53c2d26107902aa2d356eb
cecois <cecmcgee@gmail.com>
2016-03-29 07:28:44 -0400

    seed germinating ha ha

--- updaters/seedgui.php
+++ updaters/seedgui.php
@@ -4,53 +4,116 @@ if (isset($argv[1])) {
 	$which = $argv[1];
 
 } else {
-	$which = "news";}
+	// $which = "test-live";
+	$which = "live";
+}
 
 // $hardep = "408";
 // $incoming = "/Users/ccmiller/Desktop/cbb/updaters/cbb-live-json.json";
-	$incoming = "/Users/ccmiller/Desktop/cbb/updaters/cbb-" . $which . "-json.json";
+$incoming = "/Users/ccmiller/Desktop/cbb/updaters/cbb-" . $which . "-json.json";
 // $incoming = "/Users/ch23936/git/cbbutil/updaters/cbb-" . $which . "-json.json";
 
 // $BITS = array();
 // $EPS = array();
-	$BITEPS = array();
-	$allowed = ['bit', 'episode'];
+$BITEPS = array();
+$allowed = ['bit', 'episode'];
 
-	try {
+try {
 
-		$fullfile = json_decode(file_get_contents($incoming), true);
+	$fullfile = json_decode(file_get_contents($incoming), true);
 
 	// $j = 2;
-		foreach ($fullfile as $key => $value) {
+	foreach ($fullfile as $key => $value) {
 
-			$BITEPS[$value['episode']]['bits'][] = $value['bit'];
-			$BITS[] = $value['bit'];
-
-		}
-
-	} catch (Exception $e) {
-
-		var_dump($e);die();
+		$BITEPS[$value['episode']]['bits'][] = $value['bit'];
+		$BITS[] = $value['bit'];
+		$EPS[] = $value['episode'];
 
 	}
+
+} catch (Exception $e) {
+
+	var_dump($e);die();
+
+}
 
 	// first let's get master unique bits so we can get full count
-	$uniqbits = array_count_values($BITS);
+$uniqbits = array_count_values($BITS);
+
+// var_dump($uniqbits);die();
+
+$BITTOTZ = array();
+
+foreach ($uniqbits as $k=>$v) {
+	
+	$BITTOTZ[]=array("bit"=>$k,"total"=>7979);
+
+}
+
+// var_dump($BITTOTZ);die();
+// function get_bit_count($bit,$BITTOTZ){
+// 	$tbit = array_filter($BITTOTZ,function($b){
+// 		return $b["bit"]==$bit;
+// 	});
+// 	return $tbit["count"];
+// }
+
+$uniqeps = array_count_values($EPS);
 
 
-	$BITEPZ = array();
+$BITEPZ = array();
+$bitstot = 0;
 
-	foreach ($BITEPS as $key => $value) {
+foreach ($BITEPS as $key => $value) {
+
 
 // key is ep no
-		$BITEPZ['episodes'][] = array('episode' => $key, 'bits' => array_count_values($value['bits']));
+	$BITEPZ['episodes'][] = array('episode' => $key, 'bits' => array_count_values($value['bits']));
+	$bitstot = $bitstot+count($value['bits']);
 	// $BITEPZ['bitz'] = ;
 
+}
+
+// var_dump($BITEPZ);die();
+	// var_dump($uniqeps);die();
+	// $epstot = count($BITEPZ['episodes']);
+if(count($uniqeps)>1){
+	$UE=" episodes ". implode(", ", array_flip($uniqeps));
+} else {
+	$UE = " episode ".array_keys($uniqeps)[0];
+}
+
+$dump = "<p>".
+$bitstot . 
+" from "
+.$UE.":</p><ul>";
+
+foreach ($BITEPZ['episodes'] as $ep) {
+	foreach($ep['bits'] as $k => $v){
+		// $BITTOT=get_bit_count($k);
+
+		$tbit = array_filter($BITTOTZ,function($b) use ($k){
+			// echo "bbit:".$b['bit'];
+			// echo "k:".$k;
+			return $b["bit"]==$k;
+		});
+
+		echo "K:".$k."\r\n";
+		var_dump($tbit);
+		echo "------------------------>". "\r\n";
+		// var_dump($tbit[0]);die();
+		// echo json_encode($tbit[0]['total']);
+		// die();
+		$BITTOT=$tbit[0]['total'];
+
+		$dump.="<li>".$v." <em><a href='#query/episode:".$ep['episode']." AND bit:%22".urlencode($k)."%22'>".$k."</a></em> <a href='#query/bit:%22".urlencode($k)."%22'>".$BITTOT." total</a>)</li>";
+		// <li>1 new <em><a href='#query/episode:408 AND bit:%22Bottom+of+a+Deep+Pool%22'>Bottom of a Deep Pool</a></em> (<a href='#query/bit:%22Bottom+of+a+Deep+Pool%22'>7 total</a>)</li>
 	}
+}
 
-	echo json_encode($BITEPZ);
-	die();
-
+$dump.="</ul>";
+// echo $dump;
+die();
 	// $BEU = array_unique($BITEPS, SORT_REGULAR);
 	// var_dump($BITEPS);die();
 
@@ -161,4 +224,4 @@ if (isset($argv[1])) {
 
 // }
 
-	?>
+?>
