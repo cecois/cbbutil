4b13e0e126a182dd079029b02620e448d98a7bbe
Cecois <cecmcgee@gmail.com>
2017-04-15 15:44:05 -0400

    first step to all-node

---
+++ updaters/nod/js.js
@@ -0,0 +1,45 @@
+var Db = require('mongodb').Db,
+MongoClient = require('mongodb').MongoClient,
+Server = require('mongodb').Server,
+ReplSetServers = require('mongodb').ReplSetServers,
+ObjectID = require('mongodb').ObjectID,
+Binary = require('mongodb').Binary,
+GridStore = require('mongodb').GridStore,
+Grid = require('mongodb').Grid,
+Code = require('mongodb').Code,
+// FS = require('fs'),
+BSON = require('mongodb-core').BSON
+,assert = require('assert')
+,__ = require('underscore')
+// ,J2CSV = require('json2csv')
+;
+
+Config = {
+  "mongodb":"cbbbits",
+  "mongoport":"33599",
+  "mongohost":"ds033599.mongolab.com"
+}
+
+var url = 'mongodb://'+Config.mongohost+':'+Config.mongoport+'/'+Config.mongodb;
+
+var get_bitz = function(db, CEEBEE) {
+
+
+// top 10 loneliest bikes' rides
+var BZ =db.collection('bits').findOne();
+
+console.log(BZ);
+
+CEEBEE();
+
+}
+
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+
+if(err){console.log("error connecting to "+Config.mongohost+"...");} else {console.log("connected to "+Config.mongohost+", fetching bits...");}
+
+  get_bitz(db, function() {
+    db.close();
+  });
+});
