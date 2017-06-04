fa1f0777b8c0101705b8e1c16d54b51df6344e48
Cecois <cecmcgee@gmail.com>
2017-06-04 16:21:36 -0400

    processed;minor nod work

--- updaters/nod/js.js
+++ updaters/nod/js.js
@@ -1,14 +1,14 @@
 var Db = require('mongodb').Db,
-MongoClient = require('mongodb').MongoClient,
+MONGOCLIENT = require('mongodb').MongoClient,
 Server = require('mongodb').Server,
 ReplSetServers = require('mongodb').ReplSetServers,
-ObjectID = require('mongodb').ObjectID,
-Binary = require('mongodb').Binary,
-GridStore = require('mongodb').GridStore,
-Grid = require('mongodb').Grid,
-Code = require('mongodb').Code,
-// FS = require('fs'),
-BSON = require('mongodb-core').BSON
+// ObjectID = require('mongodb').ObjectID,
+// Binary = require('mongodb').Binary,
+// GridStore = require('mongodb').GridStore,
+// Grid = require('mongodb').Grid,
+// Code = require('mongodb').Code,
+FS = require('fs')
+// BSON = require('mongodb-core').BSON
 ,assert = require('assert')
 ,__ = require('underscore')
 // ,J2CSV = require('json2csv')
@@ -20,26 +20,40 @@ BSON = require('mongodb-core').BSON
 
 var url = 'mongodb://'+Config.mongouser+':'+Config.mongopsswd+'@'+Config.mongohost+':'+Config.mongoport+'/'+Config.mongodb;
 
-var get_bitz = function(db, CEEBEE) {
-
-
-var BZ =db.collection('bits').findOne();
-
-
-console.log(typeof BZ);
-
-CEEBEE();
-
+var get_bit = function(db, CEEBEE){
+	db.collection('bits').findOne()
+	.then(function(doc) {
+		if(!doc)
+			throw new Error('No record found.');
+      console.log(doc);//else case
+      CEEBEE();
+  });
 
 
 }//get_bitz
 
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
+var get_live = function(CEEBEE){
 
-if(err){console.log("error connecting to "+Config.mongohost+"...");} else {console.log("connected to "+Config.mongohost+", fetching bits...");}
+	console.log("running getlive...")
 
-  get_bitz(db, function() {
-    db.close();
-  });
-});
+	FS.readFile('../cbb-live.json', function read(err, data) {
+		if (err) {
+			throw err;
+		}
+		content = JSON.parse(data);
+
+    console.log("content:",content);   // Put all of the code here (not the best solution)
+}); //readfile
+	} //get_live
+
+// MONGOCLIENT.connect(url, function(err, db) {
+	// assert.equal(null, err);
+
+	// if(err){console.log("error connecting to "+Config.mongohost+"...");} else {console.log("connected to "+Config.mongohost+", fetching bits...");}
+
+	// get_bit(db, function() {
+	// 	db.close();
+	// });
+// });
+
+get_live()
