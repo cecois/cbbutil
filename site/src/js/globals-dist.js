c7baa6aa6b4371440ebfc1099f3db8ab54ae3d78
cecois <cecmcgee@gmail.com>
2017-08-29 21:04:31 -0400

    v2 commence

--- site/src/js/globals-dist.js
+++
@@ -1,8 +0,0 @@
-window.local = true;
-window.plierpoint=1;
-window.plierpoly=9999;
-window.plierline=999;
-window.verbose = false;
-window.dev = false;
-window.solrhost = "http://solr-lbones.rhcloud.com/";
-window.queryinit = "(episode:499 OR episode:500)";
