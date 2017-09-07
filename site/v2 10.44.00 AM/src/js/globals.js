004a347e2ff18416bf9000f7dcf6ce34b2c1e5f8
cecois <cecmcgee@gmail.com>
2017-09-06 22:11:14 -0400

    v2 code

---
+++ site/v2 10.44.00 AM/src/js/globals.js
@@ -0,0 +1,8 @@
+window.verbose = true;
+window.dev = false;
+window.local = true;
+window.plierpoint=1;
+window.plierpoly=9999;
+window.plierline=999;
+window.solrhost = "http://solr-lbones.rhcloud.com/";
+window.queryinit = "(episode:499 OR episode:500)";
---
+++ site/v2/src/js/BaseLayers-Collection.js
@@ -0,0 +1,23 @@
+var BaseLayersCollection = Backbone.Collection.extend({
+	model:BaseLayer
+	,url:null
+	,initialize:function(options){
+		options||(options={})
+		this.listenTo(appState,'change:baselayer',this.switch)
+	}
+	,switch: function(nn){
+		// var nl = (nn)?nn:appState.get("baselayer")
+		this.invoke('set',{"active":false},{silent:true})
+		return this
+		.activate(nn)
+	}
+	,activate: function(nn){
+		var nl = (nn)?nn:appState.get("baselayer")
+
+		var nm = this.findWhere({name:nl})
+		nm.set({active:true})
+		return this
+
+	}//activate
+
+})//extend
