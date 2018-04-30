31d9d3795278736ab145a1ff1a092ea76d13ef24
cecois <cecmcgee@gmail.com>
2018-04-30 17:54:32 -0400

    processed

--- site/v2/src/js/App.js
+++ site/v2/src/js/App.js
@@ -7,7 +7,7 @@ var CONFIG = {
     ,basemap:"pencil"
     ,index_root:"http://milleria.org:9200/cbb/_search?"
     // ,index_root:"http://localhost:9200/cbb/_search?"
-    ,default_query:'(bit:"Classic Jarles" OR bit:"Clumsy Portmanteau" OR bit:"Let that Cam--")'
+    ,default_query:'(episode:542)'
 }
 
 window.map = new L.Map('map',
