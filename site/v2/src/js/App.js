7910e5892b0d1369472a0c4b09cd7eddf12f75e2
cecois <cecmcgee@gmail.com>
2018-12-27 19:20:00 -0500

    proc

--- site/v2/src/js/App.js
+++ site/v2/src/js/App.js
@@ -7,7 +7,7 @@ var CONFIG = {
     ,basemap:"pencil"
     ,index_root:"http://milleria.org:9200/cbb/_search?"
     // ,index_root:"http://localhost:9200/cbb/_search?"
-    ,default_query:'(episode:579)'
+    ,default_query:'(episode:bo2018.1)'
 }
 
 window.map = new L.Map('map',
